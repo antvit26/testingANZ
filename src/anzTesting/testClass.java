d6b1b68976c0a4d3dcd894ebadaa7dab378565c3
Antony Davis <adayroor@gmail.com>
2019-12-23 13:22:48 +1300

    Add files via upload

---
+++ src/anzTesting/testClass.java
@@ -0,0 +1,67 @@
+package anzTesting;
+
+//import java.util.concurrent.TimeUnit;
+//import org.openqa.selenium.SessionNotCreatedException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.NoSuchElementException;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.remote.ProtocolHandshake;
+//import org.testng.Assert;
+//import org.testng.annotations.Test 
+
+
+public class testClass { 
+	
+	
+	WebDriver driver = new ChromeDriver();
+	//WebDriver driver = new FirefoxDriver();
+	String baseUrl = "https://digital.anz.co.nz/preauth/web/service/login";	
+	String Loginname = "82510091";
+	String Loginpassword = "Friday@97+25";
+	
+	
+	WebElement username = driver.findElement(By.id("user-id"));
+	WebElement password = driver.findElement(By.id("password"));
+	WebElement login=driver.findElement(By.id("submit"));
+	//WebElement logOff=driver.findElement(By.id("logOff"));
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\selenium\\New folder (2)\\chromedriver.exe");
+		
+		testClass t = new testClass();
+		t.testLogin();
+				
+	}
+	
+	void testLogin(){
+		
+		driver.get(baseUrl);
+		
+		username.sendKeys(Loginname);
+        password.sendKeys(Loginpassword);
+        login.click();
+        
+        String actualUrl="https://digital.anz.co.nz/preauth/web/service/login";
+        String expectedUrl= driver.getCurrentUrl();
+        //System.out.println(expectedUrl);
+        
+        if(actualUrl.equalsIgnoreCase(expectedUrl))
+        {
+            System.out.println("Test passed");
+        }
+        else
+        {
+            System.out.println("Test failed");
+        }	
+        
+        //logOff.click();
+        
+	}
+	void testTransfer() {
+		
+	}
+
+}
